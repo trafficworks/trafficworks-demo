48a408033e27b7d8a0e57cfdaf9a3afc120f477f
trafficworks <wesleybray@gmail.com>
2026-03-23 15:44:25 -0700

    Update supabase.js

--- docs/js/supabase.js
+++ docs/js/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
 const SUPABASE_URL = 'https://sgwzbuusrhbgpzanzdia.supabase.co'
-const SUPABASE_ANON_KEY = 'PASTE_YOUR_sb_publishable_KEY_HERE'
+const SUPABASE_ANON_KEY = 'sb_publishable_7CuSVpkyZBl55XaxbUKmnQ_0L3xllu7'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
